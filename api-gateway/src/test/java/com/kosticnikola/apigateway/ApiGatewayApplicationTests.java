607981df94b575e15d64ec4cafd1b30f0b483423
Nikola Kostic <nixel@pop-os.localdomain>
2021-06-24 17:41:12 +0200

    Initial commit.

---
+++ api-gateway/src/test/java/com/kosticnikola/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosticnikola.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
